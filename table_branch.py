68a1432e3e1c00ea896ea0929fcb125fabc7bcd5
Eric Alt <ericallenalt@gmail.com>
2021-02-28 12:15:48 -0600

    [feat] Add methods to update values
    resolves #1

--- table_branch.py
+++ table_branch.py
@@ -14,3 +14,27 @@ class TableBranch:
     @property
     def values(self):
         return self._values
+
+    def _update_values(self):
+        while self._liberties[1]:
+            row, col = self._liberties[1].pop()
+            assert len(self._values[row][col]) == 1
+            val = self._values[row][col].pop()
+            self._set_value(row, col, val)
+    
+    def _set_value(self, row, col, val):
+        size = len(self._values[row][col])
+        self._liberties[size].discard((row, col))
+        self._values[row][col] = val
+        for i in range(self._n):
+            self._remove_value(row, i, val)
+            self._remove_value(i, col, val)
+
+    def _remove_value(self, row, col, val):
+        if hasattr(self._values[row][col], 'remove'):
+            if val in self._values[row][col]:
+                size = len(self._values[row][col])
+                self._liberties[size].remove((row, col))
+                self._values[row][col].remove(val)
+                size = len(self._values[row][col])
+                self._liberties[size].add((row, col))
