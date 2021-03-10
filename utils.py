ebb493b06a04f959b5967ddcedbb6d67b0e0b05d
Eric Alt <ericallenalt@gmail.com>
2021-03-09 21:50:04 -0600

    [feat] Add functions to hash Latin Squares
    resolves #15

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ from collections import namedtuple
 Triple = namedtuple('Triple', ['r', 'c', 's'])
 
 
+# ----- Orthogonal Array -------------------------------------------------------
+
 def to_orthogonal_array(latin_square):
     return [Triple(r=r, c=c, s=s) for r, row in enumerate(latin_square) for c, s in enumerate(row)]
 
@@ -19,3 +21,23 @@ def _get_n(orthogonal_array):
 
 def _get_empty_latin_square(n):
     return [[None for _ in range(n)] for _ in range(n)]
+
+# ----- Hashing ----------------------------------------------------------------
+
+_hash_power = lambda r, c, n: (n ** 2 - 1) - (r * n + c)
+_hash_triple = lambda triple, n: (triple.s) * n ** _hash_power(triple.r, triple.c, n)
+
+def hash_latin_square(latin_square):
+    return hash_orthognoal_array(to_orthogonal_array(latin_square))
+
+def hash_orthognoal_array(orthogonal_array):
+    n = _get_n(orthogonal_array)
+    return sum(_hash_triple(triple, n) for triple in orthogonal_array)
+
+def from_hash_val(hash_val, n):
+    latin_square = _get_empty_latin_square(n)
+    for r in reversed(range(n)):
+        for c in reversed(range(n)):
+            latin_square[r][c] = hash_val % n
+            hash_val //= n
+    return latin_square
--- utils_test.py
+++ utils_test.py
@@ -1,6 +1,7 @@
 import unittest
 
-from utils import to_orthogonal_array, from_orthogonal_array, Triple
+import utils
+from utils import Triple
 
 
 class OperationsTest(unittest.TestCase):
@@ -11,7 +12,7 @@ class OperationsTest(unittest.TestCase):
             [1, 2, 0],
             [2, 0, 1],
         ]
-        actual = to_orthogonal_array(latin_square)
+        actual = utils.to_orthogonal_array(latin_square)
         expected = [
             Triple(r=0, c=0, s=0),
             Triple(r=0, c=1, s=1),
@@ -37,13 +38,51 @@ class OperationsTest(unittest.TestCase):
             Triple(r=2, c=1, s=0),
             Triple(r=2, c=2, s=1),
         ]
-        actual = from_orthogonal_array(orthogonal_array)
+        actual = utils.from_orthogonal_array(orthogonal_array)
         expected = [
             [0, 1, 2],
             [1, 2, 0],
             [2, 0, 1],
         ]
         self.assertListEqual(actual, expected)        
+
+    def test_hash_orthogonal_array(self):
+        orthogonal_array = [
+            Triple(r=0, c=0, s=0),
+            Triple(r=0, c=1, s=1),
+            Triple(r=0, c=2, s=2),
+            Triple(r=1, c=0, s=1),
+            Triple(r=1, c=1, s=2),
+            Triple(r=1, c=2, s=0),
+            Triple(r=2, c=0, s=2),
+            Triple(r=2, c=1, s=0),
+            Triple(r=2, c=2, s=1),
+        ]
+        
+        actual = utils.hash_orthognoal_array(orthogonal_array)
+        expected = 4069
+        self.assertEqual(actual, expected)
+
+    def test_hash_latin_square(self):
+        latin_square = [
+            [0, 1, 2],
+            [1, 2, 0],
+            [2, 0, 1],
+        ]
+        actual = utils.hash_latin_square(latin_square)
+        expected = 4069
+        self.assertEqual(actual, expected)
+
+    def test_from_hash_val(self):
+        hash_val = 4069
+        n = 3
+        actual = utils.from_hash_val(hash_val, n)
+        expected = [
+            [0, 1, 2],
+            [1, 2, 0],
+            [2, 0, 1],
+        ]
+        self.assertListEqual(actual, expected)
 
 
 if __name__ == '__main__':
